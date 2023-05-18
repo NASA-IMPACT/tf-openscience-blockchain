adc31be2d9d11eba462beadcd68530b445c5b535
xhagrg <grg.iksha@hotmail.com>
2023-05-18 14:41:17 -0500

    Add recent changes.

--- os-blockchain-api/sync-api/rest-api/datastore.js
+++ os-blockchain-api/sync-api/rest-api/datastore.js
@@ -1,37 +1,63 @@
-var AWS = require('aws-sdk');
+let AWS = require('aws-sdk');
 
-var BUCKETNAME = process.env.BUCKETNAME;
+let BUCKETNAME = process.env.BUCKETNAME;
+const connection = require("./connection.js");
+
+const MCP_ROLE = "arn:aws:iam::114506680961:role/veda-data-store-read-staging";
+
 
 class Datastore {
-    constructor() {
-        this.s3Bucket = new AWS.S3();
-    }
+  constructor() {
+  }
 
-    async upload(key, dataBuffer) {
-        let params = {
-            Body: dataBuffer,
-            Key: key,
-            Bucket: BUCKETNAME
-        };
-        const s3Response = await this.s3Bucket.putObject(params).promise();
-        let response = {
-            statusCode: 200,
-            key: key,
-            hash: s3Response.ETag.replace(/\"/g, ""),
-            bucket: BUCKETNAME
-        };
-        return response;
-    }
+  async setupAWSCreds() {
+    let roleCreds = await connection.crossAccountCredentials();
+    let mcpRoleCreds = await connection.crossAccountCredentials(roleCreds, MCP_ROLE);
+    this.s3Conn = new AWS.S3({
+      apiVersion: '2016-04-18',
+      region: "us-west-2",
+      accessKeyId: mcpRoleCreds.accessKeyId,
+      secretAccessKey: mcpRoleCreds.secretAccessKey,
+      sessionToken: mcpRoleCreds.sessionToken,
+    });
+  }
 
-    async delete(key) {
-        await this.s3Bucket.deleteObject({ Bucket: BUCKETNAME, Key: key }).promise();
-    }
+  async upload(key, dataBuffer) {
+    let params = {
+      Body: dataBuffer,
+      Key: key,
+      Bucket: BUCKETNAME
+    };
+    const s3Response = await this.s3Conn.putObject(params).promise();
+    let response = {
+      statusCode: 200,
+      key: key,
+      hash: s3Response.ETag.replace(/\"/g, ""),
+      bucket: BUCKETNAME
+    };
+    return response;
+  }
 
-    async download(key) {
-        console.log(`Fetching ${key} from ${BUCKETNAME}.`);
-        let data = await this.s3Bucket.getObject({ Key: key, Bucket: BUCKETNAME }).promise();
-        return data;
-    }
+  async delete(key) {
+    await this.s3Conn.deleteObject({ Bucket: BUCKETNAME, Key: key }).promise();
+  }
+
+  async download(key) {
+    console.log(`Fetching ${key} from ${BUCKETNAME}.`);
+    let data = await this.s3Conn.getObject({ Key: key, Bucket: BUCKETNAME }).promise();
+    return data;
+  }
+
+  async getHead(key, bucketname) {
+    const s3Response = await this.s3Conn.headObject({ Key: key, Bucket: bucketname }).promise();
+    let response = {
+      statusCode: 200,
+      key: key,
+      hash: s3Response.ETag.replace(/\"/g, ""),
+      bucket: bucketname
+    };
+    return response;
+  }
 }
 
 module.exports = Datastore;
