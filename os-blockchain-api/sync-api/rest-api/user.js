0167f4eb06bde77091df26f16be444c86ae19358
xhagrg <grg.iksha@hotmail.com>
2023-05-19 14:57:30 -0500

    Add more logs.

--- os-blockchain-api/sync-api/rest-api/user.js
+++ os-blockchain-api/sync-api/rest-api/user.js
@@ -28,6 +28,7 @@ class User {
 
   async enroll(req, res, next) {
       this.username = req.headers.username;
+      console.log(this.orgName, req.headers.orgName);
       this.orgName = req.headers.orgName || this.orgName;
       let userResponse = await connection.getRegisteredUser(
         this.username,
